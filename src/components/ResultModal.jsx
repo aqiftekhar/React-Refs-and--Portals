af670c051385cc2f4dd8084b0566f1de046a7f35
Aqdas Iftekhar <aqdasiftekhar@gmail.com>
2024-03-16 01:21:18 +0500

    Improved code by reducing states and enhanced features for players by passing states to player component.

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,14 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { remainingTime, targetTime, onReset },
   ref
 ) {
   const dialog = useRef();
+  const userLost = remainingTime <=0;
+  const formatRemainingTime = (remainingTime/1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
   useImperativeHandle(ref, () => {
     return {
       open() {
@@ -15,14 +19,15 @@ const ResultModal = forwardRef(function ResultModal(
 
   return (
     <dialog ref={dialog} className="result-modal">
-      <h2>{result}</h2>
+      {userLost && <h2>You Lost!</h2>}
+      {!userLost && <h2>Your Score {score}</h2>}
       <p>
         The target time was <strong>{targetTime} second(s)</strong>
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with <strong>{formatRemainingTime} seconds left.</strong>
       </p>
-      <form action="dialog">
+      <form action="dialog" className="result-modal" onSubmit={onReset}>
         <button>Close</button>
       </form>
     </dialog>
